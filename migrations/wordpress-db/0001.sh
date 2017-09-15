39903f6c63d14c7ef8dd71d293fa46c964c80934
Andrija Vucinic <andrija.vucinic@gmail.com>
2017-09-15 15:36:40 -0700

    remove read only for master

--- migrations/wordpress-db/0001.sh
+++ migrations/wordpress-db/0001.sh
@@ -14,7 +14,6 @@ chown -R mysql.mysql /var/lib/mysql$INST
 /etc/init.d/mysql$INST start
 /usr/local/mysql/bin/mysqladmin --defaults-file=/etc/mysql/mysql$INST.cnf -u root password 'god'
 mysql --defaults-file=/etc/mysql/mysql$INST.cnf -u root -sN  -pgod -e "create database wp;"
-mysql --defaults-file=/etc/mysql/mysql$INST.cnf -u root -sN  -pgod -e "create user 'wp'@'localhost' IDENTIFIED BY 'drupal';"
-mysql --defaults-file=/etc/mysql/mysql$INST.cnf -u root -sN  -pgod -e "GRANT ALL ON wp.* TO 'wp'@'%';"
+mysql --defaults-file=/etc/mysql/mysql$INST.cnf -u root -sN  -pgod -e "create user 'wp'@'%' IDENTIFIED BY 'drupal';"
 mysql --defaults-file=/etc/mysql/mysql$INST.cnf -u root -sN  -pgod -e "GRANT REPLICATION SLAVE ON *.* TO 'replicate'@'%' IDENTIFIED BY 'magic';"
 mysql --defaults-file=/etc/mysql/mysql$INST.cnf -u root -sN  -pgod -e "FLUSH PRIVILEGES;"
